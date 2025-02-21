1c0f6ae484df0e6d8bac6244d3275da6fb7ed2b3
jordyob03 <jelizabethob@mun.ca>
2025-02-20 20:37:12 -0330

    Handling shut down in gui file

--- gui/launch/launch.py
+++ gui/launch/launch.py
@@ -1,53 +1,25 @@
 import launch
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import RegisterEventHandler
-from launch.event_handlers import OnProcessExit
 
 def generate_launch_description():
-    gui_node = Node(
-        package='gui',  # Replace with your package name
-        executable='gui',
-        name='gui_node',
-        output='screen',
-    )
-
-    detection_visualizer_node = Node(
-        package='gui',  
-        executable='detection_visualizer',
-        name='detection_visualizer_node',
-        output='screen',
-    )
-
-    param_node = Node(
-        package='gui',
-        executable='params_window',
-        name='param_node',
-        output='screen',
-    )
-
     return LaunchDescription([
-        gui_node,
-        detection_visualizer_node,
-        param_node,
-
-        # Ensure all nodes exit when any node stops
-        RegisterEventHandler(
-            OnProcessExit(
-                target_action=gui_node,
-                on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
-            )
+        Node(
+            package='gui',  # Replace with your package name
+            executable='gui',
+            name='gui_node',
+            output='screen',
         ),
-        RegisterEventHandler(
-            OnProcessExit(
-                target_action=detection_visualizer_node,
-                on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
-            )
+        Node(
+            package='gui',  # Replace with your package name
+            executable='detection_visualizer',
+            name='detection_visualizer_node',
+            output='screen',
         ),
-        RegisterEventHandler(
-            OnProcessExit(
-                target_action=param_node,
-                on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
-            )
+        Node(
+            package='gui',
+            executable='params_window',
+            name='param_node',
+            output='screen',
         ),
     ])
